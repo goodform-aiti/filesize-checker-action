3adf82e943f694d189136bfadc8dbdf6a4a0f689
Amir Alian <amircup2006@gmail.com>
2020-12-03 14:13:38 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+ERROR=0
+
+
+echo " ************** MODIFIED FILES"
+
+printf ${MODIFIED_FILES}
+
+printf "\n*****************************\n"
+
+PATHS=$(printf ${MODIFIED_FILES} | tr \\n '\n')
+
+
+echo "$PATHS" | while read FILE ; do
+    if [[ ! -f $FILE ]]
+    then
+      # skip deleted files
+      continue
+    fi
+    IS_FILE_BINARY=$(find $FILE -type f | perl -lne 'print if -B' | wc -l)
+    if [[ ! $IS_FILE_BINARY ]]
+    then
+      continue
+    fi
+
+    FILE_SIZE=$(stat -c%s index.php)
+    if [[ $FILE_SIZE > $MAX_FILE_SIZE ]]
+    then
+      echo "The file size(${FILE}) is exceed ${MAX_FILE_SIZE} bytes."
+      exit 101
+    fi
+done
